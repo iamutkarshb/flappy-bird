7ded6f555d49a1805c7feb7cff8a4cb89f46c932
keyboard-king <utkarshbansal01@gmail.com>
2021-07-05 15:30:05 +0530

    animations pending

--- flappy.py
+++ flappy.py
@@ -3,12 +3,18 @@ import pygame,sys
 pygame.init()
 
 screen = pygame.display.set_mode((288,512))
-clock = pygame.time.clock()
+clock = pygame.time.Clock()
+bg_surface =pygame.image.load("assets/background-day.png").convert()
+base_surface =pygame.image.load("assets/base.png").convert()
+base_x_pos = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    screen.blit(bg_surface,(0,0))
+    base_x_pos -=1
+    screen.blit(base_surface,(base_x_pos,450))
     pygame.display.update()
     clock.tick(120)
