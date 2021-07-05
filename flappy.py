8e3b1cff60417a949be1fc99cd1bfb73aedbc1cf
keyboard-king <utkarshbansal01@gmail.com>
2021-07-05 14:26:40 +0530

    framerate and setup completed

--- flappy.py
+++ flappy.py
@@ -3,6 +3,7 @@ import pygame,sys
 pygame.init()
 
 screen = pygame.display.set_mode((288,512))
+clock = pygame.time.clock()
 
 while True:
     for event in pygame.event.get():
@@ -10,3 +11,4 @@ while True:
             pygame.quit()
             sys.exit()
     pygame.display.update()
+    clock.tick(120)
