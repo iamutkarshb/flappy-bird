ac6834f1ab5b862459b8b27f64369ff05aaea9cf
keyboard-king <utkarshbansal01@gmail.com>
2021-07-07 02:16:49 +0530

    bird jump control added

--- flappy.py
+++ flappy.py
@@ -8,6 +8,10 @@ def draw_base():
 
 screen = pygame.display.set_mode((288,512))
 clock = pygame.time.Clock()
+
+gravity = 0.25
+bird_movement = 0
+
 bg_surface =pygame.image.load("assets/background-day.png").convert()
 base_surface =pygame.image.load("assets/base.png").convert()
 base_x_pos = 0
@@ -20,7 +24,17 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird_movement = 0
+                bird_movement -= 6
+                    
     screen.blit(bg_surface,(0,0))
+    
+    bird_movement += gravity
+    bird_rect.centery +=bird_movement
+
+
     screen.blit(bird_surface,bird_rect)
     base_x_pos -=1
     draw_base()
