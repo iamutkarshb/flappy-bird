153f48ba7169f2570e905fe2f7bb6ba3d6f030a1
keyboard-king <utkarshbansal01@gmail.com>
2021-07-07 02:10:02 +0530

    bird added

--- flappy.py
+++ flappy.py
@@ -12,12 +12,16 @@ bg_surface =pygame.image.load("assets/background-day.png").convert()
 base_surface =pygame.image.load("assets/base.png").convert()
 base_x_pos = 0
 
+bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert()
+bird_rect = bird_surface.get_rect(center = (50,256))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     screen.blit(bg_surface,(0,0))
+    screen.blit(bird_surface,bird_rect)
     base_x_pos -=1
     draw_base()
     if base_x_pos<=-288:
